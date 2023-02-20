0be41199c3ee79cdf476b99ee91a23b0ebf2184b
Nebojsa Markovic <nebojsa91markovic@gmail.com>
2023-02-20 22:05:09 +0100

    started using useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Counter from "./components/Counter";
+import Counter1 from "./components/Counter1";
 
 function App() {
+  const [show, setShow] = useState(true);
+  // const [number, setNumber] = useState(1);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Counter /> */}
+      {show ? <Counter1 /> : null}
+      {/* ternary operator */}
+      <button onClick={() => setShow(!show)}>{show ? "Hide" : "Show"}</button>
+
+      {/* ternary operator nesting */}
+      {/* <button onClick={() => setNumber(() => number + 1)}>
+        {number === 1 ? "jedan" : number % 2 === 0 ? "dva" : "tri"}
+      </button> */}
     </div>
   );
 }
